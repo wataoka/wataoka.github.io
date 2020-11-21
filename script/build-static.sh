d7ee031f3e9e719760a19a4060bbd3bcb07c075a
wataoka <wataoka@stu.kobe-u.ac.jp>
2020-11-21 14:16:56 +0900

    remove: docs in node_modules

--- script/build-static.sh
+++ script/build-static.sh
@@ -7,8 +7,12 @@ base=".."
 # build static file
 python "${base}/freeze.py"
 
-# rename build folder to docs
+# rename build to docs
+rm -rf ${base}/docs
 mv ${base}/build ${base}/docs
+
+# remove other docs dir
+rm -rf ${base}/docs/static/node_modules/bootstrap-4.1.3/site/docs
 
 # show page
 open "${base}/docs/index.html"
