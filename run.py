1fadb907d83dcf5fda71f7e93642349c3acc6720
koki.wataoka <koki.wataoka@linecorp.com>
2021-12-13 19:23:12 +0900

    Clean: run.py

--- run.py
+++ run.py
@@ -4,21 +4,25 @@ import argparse
 
 from flask import Flask, render_template, url_for
 
+
 app = Flask(__name__, static_url_path='/static')
 works_json_path = os.path.join(app.static_folder, 'json/works.json')
 careers_json_path = os.path.join(app.static_folder, 'json/careers.json')
 skills_json_path = os.path.join(app.static_folder, 'json/skills.json')
 studies_json_path = os.path.join(app.static_folder, 'json/studies.json')
 
+
 @app.route('/')
 def home():
     return render_template('/index.html')
+
 
 @app.route('/portfolio.html')
 def portfolio():
     with open(works_json_path) as works_json_file:
         works_list = json.load(works_json_file)
     return render_template('/portfolio.html', works_list=works_list)
+
 
 @app.route('/work/<work_id>.html')
 def work(work_id):
@@ -27,17 +31,20 @@ def work(work_id):
     work = works_list[int(work_id)]
     return render_template('/portfolio/work.html', work=work)
 
+
 @app.route('/career.html')
 def career():
     with open(careers_json_path) as careers_json_file:
         careers_list = json.load(careers_json_file) 
     return render_template('/career.html', careers_list=careers_list)
 
+
 @app.route('/study.html')
 def study():
     with open(studies_json_path) as studies_json_file:
         studies_list = json.load(studies_json_file) 
     return render_template('/study.html', studies_list=studies_list)
+
 
 @app.route('/skill.html')
 def skill():
@@ -62,21 +69,21 @@ def skill():
 
     return render_template('/skill.html', language_dict=language_dict, framework_dict=framework_dict, machine_learning_dict=machine_learning_dict, other_dict=other_dict)
 
+
 @app.route('/paper.html')
 def paper():
     return render_template('/paper.html')
+
 
 @app.route('/contact.html')
 def contact():
     return render_template('/contact.html')
 
+
 @app.route('/resume.html')
 def resume():
     return render_template('/resume.html')
 
-@app.route('/answer/test')
-def answer():
-    return 'これはテストアンサーです。'
 
 def parse_args():
     parser = argparse.ArgumentParser()
