df68a49f35957d0e6b3b453ba392487034adc45c
wataoka <wataoka.koki@gmail.com>
2024-11-17 19:09:37 +0900

    Fix: Abstract

--- src/components/Abstract.vue
+++ src/components/Abstract.vue
@@ -2,9 +2,9 @@
   <div class="abstract mt-20 px-10">
     <h1>Abstract</h1>
     <div class="bg-white rounded-md mt-1 px-6 py-4 shadow-md">
-        Hello, I'm Wataoka. 
-        <p class="mt-2">I mainly work on the safety of LLM and VLM.</p>
-        <p class="mt-2">Nece to meet you.</p>
+        Hi, I'm Wataoka. 
+        <p class="mt-2">I am involved in research and development focused on the safety of large language models (LLMs). My work includes projects such as AI Red Teaming, Guardrails, Safety Tuning, and Safety Benchmarking. In the past, I have also conducted research on topics like speech processing and fairness in machine learning.</p>
+        <p class="mt-2">Nece to meet you!</p>
     </div>
   </div>
 </template>
